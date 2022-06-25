a6e1a2a52c48fdee10a3b41fda44055f6b99b395
Nikhil <nikhilg1602@gmail.com>
2022-06-25 18:55:58 +0530

    initial commit

--- to-do list/todo.js
+++ to-do list/todo.js
@@ -27,15 +27,15 @@ window.addEventListener('load', () => {
 		const task_actions_el = document.createElement('div');
 		task_actions_el.classList.add('actions');
 
-		const task_edit_el = document.createElement('button');
-		task_edit_el.classList.add('edit');
-		task_edit_el.innerText = 'Edit';
+		const task_check = document.createElement('button');
+		task_check.classList.add('check');
+		task_check.innerText = 'check';
 
 		const task_delete_el = document.createElement('button');
 		task_delete_el.classList.add('delete');
 		task_delete_el.innerText = 'Delete';
 
-		task_actions_el.appendChild(task_edit_el);
+		task_actions_el.appendChild(task_check);
 		task_actions_el.appendChild(task_delete_el);
 
 		task_el.appendChild(task_actions_el);
@@ -44,15 +44,10 @@ window.addEventListener('load', () => {
 
 		input.value = '';
 
-		task_edit_el.addEventListener('click', (e) => {
-			if (task_edit_el.innerText.toLowerCase() == "edit") {
-				task_edit_el.innerText = "Save";
-				task_input_el.removeAttribute("readonly");
-				task_input_el.focus();
-			} else {
-				task_edit_el.innerText = "Edit";
-				task_input_el.setAttribute("readonly", "readonly");
-			}
+		task_check.addEventListener('click', (e) => {
+			task_content_el.style.textDecoration="line-through";
+			task_el.style.backgroundColor="green";
+
 		});
 
 		task_delete_el.addEventListener('click', (e) => {
